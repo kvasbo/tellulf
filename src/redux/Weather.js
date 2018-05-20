047be590ef090ade246dc8588e46fc58cc1b0347
Audun Kvasbø <audun@kvasbo.no>
2018-05-20 15:57:32 +0200

    Weather reducer now auto-filters on two days

--- src/redux/Weather.js
+++ src/redux/Weather.js
@@ -1,4 +1,5 @@
-import { cloneDeep } from 'lodash';
+import { cloneDeep, omit } from 'lodash';
+import Moment from 'moment';
 import { UPDATE_WEATHER, PRUNE_WEATHER, UPDATE_WEATHER_LIMITS, UPDATE_WEATHER_LONG } from './actions';
 
 const initialState = {
@@ -10,9 +11,17 @@ const initialState = {
 export function Weather(state = initialState, action) {
   switch (action.type) {
     case UPDATE_WEATHER: {
-      const newWeather = cloneDeep(state);
-      newWeather.weather = { ...newWeather.weather, ...action.data }
-      return newWeather
+      const from = Moment().startOf('day');
+      const to = Moment().add(2, 'day').startOf('day');
+      const toFilter = Object.values({ ...state.weather, ...action.data });
+      const filtered = toFilter.filter((w) => {
+        return Moment(w.time).isBetween(from, to, null, "[]");
+      });
+      const newWeather = {};
+      filtered.forEach((w) => {
+        newWeather[w.time] = w;
+      });
+      return { ...state, weather: newWeather }
     }
     case UPDATE_WEATHER_LONG: {
       const newWeather = cloneDeep(state);
