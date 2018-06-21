f26ecc0dc5754e9163fcd5530ace8d3459f10751
kvasbo <audun@kvasbo.no>
2018-06-21 20:08:15 +0200

    Only solceller and weather!

--- src/Tellulf.jsx
+++ src/Tellulf.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import Moment from 'moment';
 import ErrorBoundary from './ErrorBoundary';
-import Klokke from './klokke/Klokke';
-import Netatmo from './netatmo/Netatmo';
-import Kalender from './kalender/Kalender';
-import Ruter from './ruter/Ruter';
-import Smarthus from './smarthus/Smarthus';
 import Solceller from './solceller/Solceller';
 import Yr from './weather/Yr';
 import './cssReset.css';
@@ -22,50 +17,22 @@ function startReloadLoop() {
 }
 
 class Tellulf extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { modalBlinds: false };
-  }
-
   componentDidMount() {
     startReloadLoop();
   }
 
   render() {
     return (
-      <div className="gridContainer">
-        <div style={{ gridColumnStart: 1, gridColumnEnd: 4, gridRowStart: 1, gridRowEnd: 2 }}>
-          <ErrorBoundary><Klokke /></ErrorBoundary>
-        </div>
-        <div style={{ gridColumnStart: 1, gridColumnEnd: 4, gridRowStart: 2, gridRowEnd: 3 }} className="block">
-          <ErrorBoundary><Netatmo /></ErrorBoundary>
-        </div>
-        <div style={{ gridColumnStart: 1, gridColumnEnd: 4, gridRowStart: 3, gridRowEnd: 4 }} className="block">
+      <div className="gridContainer" style={{ flexDirection: 'column' }}>
+        <div style={{ flex: 1 }} className="block">
           <ErrorBoundary><Solceller /></ErrorBoundary>
         </div>
-        <div style={{ gridColumnStart: 1, gridColumnEnd: 4, gridRowStart: 4, gridRowEnd: 5 }} className="block">
+        <div style={{ flex: 1 }} className="block">
           <ErrorBoundary><Yr /></ErrorBoundary>
-        </div>
-        <div style={{ gridColumnStart: 4, gridColumnEnd: 5, gridRowStart: 1, gridRowEnd: 5 }} className="block">
-          <ErrorBoundary><Kalender /></ErrorBoundary>
-        </div>
-        <div style={{ gridColumnStart: 1, gridColumnEnd: 4, gridRowStart: 5, gridRowEnd: 6 }} className="block">
-          <ErrorBoundary><Ruter stasjon="3012315" retning="1 (Retning sentrum)" /></ErrorBoundary>
-        </div>
-        <div style={{ gridColumnStart: 4, gridColumnEnd: 5, gridRowStart: 5, gridRowEnd: 6 }} className="block">
-          <ErrorBoundary><Smarthus /></ErrorBoundary>
         </div>
       </div>
     );
   }
 }
-
-/*
-
-<div style={{ gridColumnStart: 3, gridColumnEnd: 4, gridRowStart: 5, gridRowEnd: 6 }} className="block">
-          <ErrorBoundary><Smarthus /></ErrorBoundary>
-        </div>
-
-        */
 
 export default Tellulf;
