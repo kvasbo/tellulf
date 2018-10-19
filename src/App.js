e309ec6e2bfcefdfbe0dbb05aea4bcc5dfa2db11
kvasbo <audun@kvasbo.no>
2018-10-19 14:46:55 +0200

    Add auth to firebase

--- src/App.js
+++ src/App.js
@@ -11,6 +11,23 @@ Moment.locale('nb');
 window.firebase = firebase;
 
 class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false };
+  }
+
+  componentDidMount() {
+    firebase.auth().signInAnonymously();
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      }
+    });
+  }
+
+  
+
   getApp() {
     return (
       <Provider store={store}>
@@ -19,7 +36,14 @@ class App extends React.PureComponent {
     );
   }
 
+  getLoggingIn() {
+    return (
+      <div>Logger inn...</div>
+    );
+  }
+
   render() {
+    if (!this.state.loggedIn) return this.getLoggingIn();
     return this.getApp();
   }
 }
