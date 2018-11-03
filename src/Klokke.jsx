510de75952f6f46062590b2ac1be4f80b5ab8de2
kvasbo <audun@kvasbo.no>
2018-11-03 17:54:56 +0100

    Klokka klar

--- src/Klokke.jsx
+++ src/Klokke.jsx
@@ -15,9 +15,12 @@ class Clock extends React.PureComponent {
 
   render() {
     return (
-      <div style={{ paddingBottom: 20, paddingTop: 20, backgroundColor: '#000000', justifyContent: 'center', alignItems: 'center' }} >
-        <div style={{ color: '#ffffff', fontSize: 100, fontWeight: '200' }}>{this.state.time.format("HH:mm")}</div>
-        <div style={{ color: '#ffffff', fontSize: 40, fontWeight: '200' }}>{this.state.time.format("dddd Do MMMM")}</div>
+      <div style={{
+        display: 'flex', flexDirection: 'column', paddingBottom: 20, paddingTop: 20, backgroundColor: '#000000', justifyContent: 'center', alignItems: 'center',
+      }}
+      >
+        <div style={{ color: '#ffffff', fontSize: 100, fontWeight: '200' }}>{this.state.time.format('HH:mm')}</div>
+        <div style={{ color: '#ffffff', fontSize: 40, fontWeight: '200' }}>{this.state.time.format('dddd Do MMMM')}</div>
       </div>
     );
   }
