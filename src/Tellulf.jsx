e791dce70caac182a5f7727e8d955a291f2db8ba
kvasbo <audun@kvasbo.no>
2018-11-03 15:26:07 +0100

    Ruter tilbake i folden

--- src/Tellulf.jsx
+++ src/Tellulf.jsx
@@ -3,6 +3,7 @@ import Moment from 'moment';
 import ErrorBoundary from './ErrorBoundary';
 import Solceller from './solceller/Solceller';
 import Yr from './weather/Yr';
+import Ruter from './ruter/Ruter';
 import './cssReset.css';
 import './tellulf.css';
 
@@ -22,11 +23,14 @@ class Tellulf extends React.PureComponent {
   render() {
     return (
       <div className="grid">
-        <div style={{ gridColumn: '1 / 1', gridRow: '3 / 3' }} className="block">
+        <div style={{ gridColumn: '1 / 1', gridRow: '3 / 4' }} className="block">
           {this.props.loggedIn && <ErrorBoundary><Solceller /></ErrorBoundary>}
         </div>
-        <div style={{ gridColumn: '1 / 1', gridRow: '4 / 4' }} className="block">
+        <div style={{ gridColumn: '1 / 1', gridRow: '4 / 5' }} className="block">
           <ErrorBoundary><Yr /></ErrorBoundary>
+        </div>
+        <div style={{ gridColumn: '1 / 3', gridRow: '5 / 5' }} className="block">
+          <ErrorBoundary><Ruter stasjon="3012315" retning="1 (Retning sentrum)" /></ErrorBoundary>
         </div>
       </div>
     );
