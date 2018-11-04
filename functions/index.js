39bf4140a08f1686e5b6a17ab0d3bce5ce9e4d47
kvasbo <audun@kvasbo.no>
2018-11-04 09:08:59 +0100

    Proxy v1

--- functions/index.js
+++ functions/index.js
@@ -1,45 +1,18 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
+const express = require('express');
 
-const port = 3000;
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-exports.proxy = functions.https.onRequest((request, response) => {
-  
-  axios.get(req.params.url).then(data => {
-    response.set("Access-Control-Allow-Origin", "*");
-    response.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    response.send(data.data);
-  }).catch(err => console.error(err));
-
-});
-
-/*
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('get', req.params);
-  next();
-});
-
-app.get('/:url', async (req, res) => {
-  try {
-    const response = await axios.get(req.params.url);
-    res.send(response.data);
-  } catch (err) {
+app.get('/:url', (req, res) => {
+  console.log('Get', req.params.url);
+  axios.get(req.params.url).then((data) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.send(data.data);
+  }).catch((err) => {
     console.log(err);
-  }
+  });
 });
 
-app.listen(port, () => console.log(`Proxy listening on port ${port}!`))
-*/
+exports.proxy = functions.https.onRequest(app);
