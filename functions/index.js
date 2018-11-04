69fe3d40adbc2612295c0006bbf0ad117aa02a76
kvasbo <audun@kvasbo.no>
2018-11-04 09:30:56 +0100

    Fix?

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@ const app = express();
 app.get('/:url', (req, res) => {
   console.log('Get', req.params.url);
   axios.get(req.params.url).then((data) => {
-    res.set("Access-Control-Allow-Origin", "*");
-    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.send(data.data);
   }).catch((err) => {
     console.log(err);
