5e92b646ffbd1b8244e89f066c54265322b97b37
kvasbo <audun@kvasbo.no>
2018-11-28 14:11:36 +0100

    TS Fixes

--- src/redux/Weather.ts
+++ src/redux/Weather.ts
@@ -4,6 +4,28 @@ import minBy from 'lodash/minBy';
 import SunCalc from 'suncalc';
 import { UPDATE_WEATHER } from './actions';
 
+interface state {
+  weather: {} | undefined,
+  long: {},
+  limits: {} | undefined,
+  lat: number | undefined,
+  lon: number | undefined,
+  todayMinMax: { min: number | undefined, max: number | undefined },
+}
+
+interface weatherData {
+  temp: number | null,
+  rain: number | null,
+  rainMin: number | null,
+  rainMax: number | null,
+  clouds: number | null,
+  wind: number | null,
+  symbol: string | null,
+  symbolNumber: number | null,
+  sunHeight: number | null,
+  time: string | number,
+}
+
 const initialState = {
   weather: undefined,
   long: {},
@@ -13,18 +35,20 @@ const initialState = {
   todayMinMax: { min: undefined, max: undefined },
 };
 
-export default function Weather(state = initialState, action) {
+export default function Weather(state: state = initialState, action: { type: string, data: any, lat: number, lon: number }) {
   switch (action.type) {
     case UPDATE_WEATHER: {
       const from = Moment().startOf('day');
       const to = Moment().add(3, 'day').startOf('day');
-      const toFilter = Object.values({ ...state.weather, ...action.data.weather });
+      const toFilter = Object.values({ ...state.weather as weatherData, ...action.data.weather as weatherData });
       const filtered = toFilter.filter((w) => {
-        return Moment(w.time).isBetween(from, to, null, '[]');
+        if (!w) return false;
+        return Moment(w['time']).isBetween(from, to, undefined, '[]');
       });
       const newWeather = {};
       filtered.forEach((w) => {
-        newWeather[w.time] = w;
+        if (!w) return false;
+        newWeather[w['time']] = w;
       });
       return {
         ...state,
@@ -54,11 +78,10 @@ function parseLimits(data, lat, long) {
   const minTempTime = minTempPoint.time;
   const roundedMin = Math.floor((minTemp - 2) / 10) * 10;
   const roundedMax = Math.ceil((maxTemp + 2) / 10) * 10;
-
   const lowerRange = Math.min(0, roundedMin);
   const upperRange = Math.max(roundedMin + 30, roundedMax);
 
-  const ticks = [];
+  const ticks: number[] = [];
   for (let i = lowerRange; i <= upperRange; i += 10) {
     ticks.push(i);
   }
@@ -71,14 +94,14 @@ function parseLimits(data, lat, long) {
 }
 
 function getSunMeta(lat, long) {
-  const now = new Moment();
-  const yesterday = new Moment(now).subtract(1, 'days');
+  const now = Moment();
+  const yesterday = Moment(now).subtract(1, 'days');
   const sunTimes = SunCalc.getTimes(new Date(), lat, long);
   const sunTimesYesterday = SunCalc.getTimes(yesterday.toDate(), lat, long);
-  const sunriseM = new Moment(sunTimes.sunrise);
-  const sunsetM = new Moment(sunTimes.sunset);
-  const sunriseYesterday = new Moment(sunTimesYesterday.sunrise);
-  const sunsetYesterday = new Moment(sunTimesYesterday.sunset);
+  const sunriseM = Moment(sunTimes.sunrise);
+  const sunsetM = Moment(sunTimes.sunset);
+  const sunriseYesterday = Moment(sunTimesYesterday.sunrise);
+  const sunsetYesterday = Moment(sunTimesYesterday.sunset);
   const diffRise = sunriseM.diff(sunriseYesterday, 'minutes') - 1440;
   const diffSet = sunsetM.diff(sunsetYesterday, 'minutes') - 1440;
   const sunrise = sunriseM.valueOf();
