a2e9044527eff1e0b600889833b914cd23fba024
kvasbo <audun@kvasbo.no>
2019-03-19 19:19:19 +0100

    Sentry test 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Moment from 'moment';
 import { Provider } from 'react-redux';
-import * as Sentry from '@sentry/browser';
 import 'moment/locale/nb';
 import Tellulf from './Tellulf';
 import firebase from './firebase.ts';
@@ -9,10 +8,6 @@ import { store } from './redux/store.ts';
 import tibberUpdater from './tibberUpdater.ts';
 
 Moment.locale('nb');
-
-Sentry.init({
-  dsn: "https://ab9aa32b39ab4fc88b0500959309d66a@sentry.io/1412935"
-});
 
 window.firebase = firebase;
 const tibber = new tibberUpdater(store);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import './index.css';
-import App from './App.js';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+Sentry.init({
+  dsn: 'https://ab9aa32b39ab4fc88b0500959309d66a@sentry.io/1412935',
+});
+
+Sentry.captureMessage('Tellulf loaded');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
