fe762a947466814015ba04b0255cc14d62fc5627
kvasbo <audun@kvasbo.no>
2019-04-08 20:59:51 +0200

    Tallpanel er nå egen (to)komponent

--- src/solceller/TallPanel.jsx
+++ src/solceller/TallPanel.jsx
@@ -5,14 +5,37 @@ import TallPanelDisplay from './TallPanelDisplay';
 
 class TallPanel extends React.PureComponent {
   render() {
+    const currentPower = this.props.realtimePower.power + this.props.currentSolar.now; // Find actual current usage
+    // Todo: Fix percentage
+    const producedPercent = (this.props.realtimePower.accumulatedConsumption > 0) ? (this.props.currentSolar.today / 10) / this.props.realtimePower.accumulatedConsumption : 0;
+
     return (
-      <TallPanelDisplay />
+      <TallPanelDisplay
+        currentPower={Math.round(currentPower)}
+        currentProduction={Math.round(this.props.currentSolar.now)}
+        currentConsumption={this.props.realtimePower.power}
+        producedPercent={Math.round(producedPercent)}
+        accumulatedConsumption={Math.round(this.props.realtimePower.accumulatedConsumption * 100) / 100}
+        consumptionMinimum={this.props.realtimePower.minPower}
+        consumptionMaximum={this.props.realtimePower.maxPower}
+        consumptionAverage={this.props.realtimePower.averagePower}
+        localProductionDay={getRoundedNumber(Number(this.props.currentSolar.today) / 1000)}
+        localProductionMonth={getRoundedNumber(Number(this.props.currentSolar.month) / 1000)}
+        localProductionYear={getRoundedNumber(Number(this.props.currentSolar.year) / 1000)}
+        localProductionTotal={getRoundedNumber(Number(this.props.currentSolar.total) / 1000)}
+        localProductionMaxDay={this.props.max.maxDay}
+        localProductionMaxMonth={this.props.max.maxMonth}
+        localProductionMaxYear={this.props.max.maxYear}
+        localProductionMaxTotal={this.props.max.maxEver}
+      />
     );
   }
 }
 
 TallPanel.propTypes = {
   currentSolar: PropTypes.object.isRequired,
+  realtimePower: PropTypes.object.isRequired,
+  max: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => {
@@ -27,3 +50,14 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(TallPanel);
+
+function getRoundedNumber(number) {
+  if (number < 10) {
+    return number.toFixed(3);
+  } if (number < 100) {
+    return number.toFixed(2);
+  } if (number < 1000) {
+    return number.toFixed(1);
+  }
+  return number.toFixed(0);
+}
--- src/solceller/TallPanelDisplay.jsx
+++ src/solceller/TallPanelDisplay.jsx
@@ -1,8 +1,108 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import './solceller.css';
 
 class TallPanelDisplay extends React.PureComponent {
-
+  render() {
+    return (
+      <div style={{
+        flex: 4.5, display: 'flex', flexDirection: 'column', alignItems: 'space-evenly',
+      }}
+      >
+        <div className="energyTableRow">
+          <div className="energyTableBox energyTableBoxLarge">
+            <span className="smallStyle">reelt forbruk</span>
+            {this.props.currentPower}W
+          </div>
+          <div className="energyTableBox energyTableBoxLarge">
+            <span className="smallStyle">produksjon</span>
+            {this.props.currentProduction}W
+          </div>
+          <div className="energyTableBox energyTableBoxLarge">
+            <span className="smallStyle">betalt forbruk</span>
+            {this.props.currentConsumption}W
+          </div>
+          <div className="energyTableBox energyTableBoxLarge">
+            <span className="smallStyle">produsert %</span>
+            {this.props.producedPercent}%
+          </div>
+        </div>
+        <div className="energyTableRow">
+          <div className="energyTableBox">
+            <span className="smallStyle">fakturert dag</span>
+            {this.props.accumulatedConsumption}kWh
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">bruk min</span>
+            {this.props.consumptionMinimum}W
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">bruk snitt</span>
+            {this.props.consumptionAverage}W
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">bruk max</span>
+            {this.props.consumptionMaximum}W
+          </div>
+        </div>
+        <div className="energyTableRow">
+          <div className="energyTableBox">
+            <span className="smallStyle">prod dag</span>
+            {this.props.localProductionDay}kWh
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">prod måned</span>
+            {this.props.localProductionMonth}kWh
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">prod år</span>
+            {this.props.localProductionYear}kWh
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">prod totalt</span>
+            {this.props.localProductionTotal}kWh
+          </div>
+        </div>
+        <div className="energyTableRow">
+          <div className="energyTableBox">
+            <span className="smallStyle">max dag</span>
+            {this.props.localProductionMaxDay}W
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">max måned</span>
+            {this.props.localProductionMaxMonth}W
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">max år</span>
+            {this.props.localProductionMaxYear}W
+          </div>
+          <div className="energyTableBox">
+            <span className="smallStyle">max totalt</span>
+            {this.props.localProductionMaxTotal}W
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+TallPanelDisplay.propTypes = {
+  currentPower: PropTypes.number.isRequired,
+  currentProduction: PropTypes.number.isRequired,
+  currentConsumption: PropTypes.number.isRequired,
+  producedPercent: PropTypes.number.isRequired,
+  accumulatedConsumption: PropTypes.number.isRequired,
+  consumptionMinimum: PropTypes.number.isRequired,
+  consumptionAverage: PropTypes.number.isRequired,
+  consumptionMaximum: PropTypes.number.isRequired,
+  localProductionDay: PropTypes.string.isRequired,
+  localProductionMonth: PropTypes.string.isRequired,
+  localProductionYear: PropTypes.string.isRequired,
+  localProductionTotal: PropTypes.string.isRequired,
+  localProductionMaxDay: PropTypes.number.isRequired,
+  localProductionMaxMonth: PropTypes.number.isRequired,
+  localProductionMaxYear: PropTypes.number.isRequired,
+  localProductionMaxTotal: PropTypes.number.isRequired,
+};
 
 export default TallPanelDisplay;
