071f06473aec7b0104fd38dea843480c198ef2e8
kvasbo <audun@kvasbo.no>
2019-05-01 19:33:10 +0200

    Fixed the crash bug. Seems like you need to run the software before pushing.

--- src/kalender/Dag.jsx
+++ src/kalender/Dag.jsx
@@ -6,43 +6,58 @@ import HendelseMedTid from './HendelseMedTid';
 
 class Dag extends React.PureComponent {
   getDinner() {
-    if (!this.props.dinner) return null;
-    return (
-      <div style={{ paddingLeft: 15 }}>{this.props.dinner.events[0].name}</div>
-    );
+    try {
+      if (!this.props.dinner || !this.props.dinner.events) return null;
+      return (
+        <div style={{ paddingLeft: 15 }}>{this.props.dinner.events[0].name}</div>
+      );
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
   }
 
   getBirthdays() {
-    if (!this.props.birthdays) return null;
+    if (!this.props.birthdays || !this.props.birthdays.events) return null;
     const out = [];
-    // console.log(this.props.birthdays);
-    this.props.birthdays.events.forEach((b) => {
-      const matches = b.name.match(/\d+$/);
-      let name = { b };
-      if (matches) {
-        const number = parseInt(matches[0], 10);
-        const age = new Date().getFullYear() - number;
-        name = name.substring(0, name.length - 5);
-        name = `${name} (${age})`;
-      }
-      out.push(<div style={{ padding: 10 }} key={b.id}>{name}</div>);
-    });
+    try {
+      this.props.birthdays.events.forEach((b) => {
+        const matches = b.name.match(/\d+$/);
+        // eslint-disable-next-line prefer-destructuring
+        let name = b.name;
+        if (matches) {
+          const number = parseInt(matches[0], 10);
+          const age = new Date().getFullYear() - number;
+          name = name.substring(0, name.length - 5);
+          name = `${name} (${age})`;
+        }
+        out.push(<div style={{ padding: 10 }} key={b.id}>{name}</div>);
+      });
+    } catch (err) {
+      console.log(err);
+    }
     return out;
   }
 
   getEvents() {
-    if (!this.props.events) return null;
-    const events = this.props.events.events.sort((a, b) => {
-      return (a.start.isBefore(b.start)) ? -1 : 1;
-    });
+    if (!this.props.events || !this.props.events.events) return null;
     const out = [];
-    events.forEach((e) => {
-      if (e.fullDay) {
-        out.push(<HendelseFullDag key={e.id} data={e} />);
-      } else {
-        out.push(<HendelseMedTid key={e.id} data={e} />);
-      }
-    });
+
+    try {
+      const events = this.props.events.events.sort((a, b) => {
+        return (a.start.isBefore(b.start)) ? -1 : 1;
+      });
+
+      events.forEach((e) => {
+        if (e.fullDay) {
+          out.push(<HendelseFullDag key={e.id} data={e} />);
+        } else {
+          out.push(<HendelseMedTid key={e.id} data={e} />);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
     return out;
   }
 
@@ -60,9 +75,9 @@ class Dag extends React.PureComponent {
 }
 
 Dag.defaultProps = {
-  dinner: {},
-  birthdays: {},
-  events: {},
+  dinner: undefined,
+  birthdays: undefined,
+  events: undefined,
 };
 
 Dag.propTypes = {
