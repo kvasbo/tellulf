10f0dfd0f5af7d1b73cb7389dc2c71116b1e67ab
kvasbo <audun@kvasbo.no>
2019-05-05 07:48:24 +0200

    Fixed netatmo averages

--- src/redux/NetatmoAverages.ts
+++ src/redux/NetatmoAverages.ts
@@ -1,9 +1,34 @@
 import { NETATMO_UPDATE_AVERAGES } from './actions';
 
-export default function NetatmoAverages(state = {}, action: { type: string, data: object }) {
+export interface NetatmoAverages {
+  humidity: number;
+  pressure: number;
+  temperature: number;
+  time: number;
+}
+
+const initState: NetatmoAverages = {
+  humidity: 0,
+  pressure: 0,
+  temperature: 0,
+  time: 0,
+}
+
+export default function NetatmoAverages(state = initState, action: { type: string, data: NetatmoAverages }) : NetatmoAverages {
   switch (action.type) {
     case NETATMO_UPDATE_AVERAGES: {
-      return { ...state, ...action.data };
+      console.log(action.data);
+      try {
+        let newState: NetatmoAverages = state;
+        newState.humidity = action.data.humidity;
+        newState.pressure = action.data.pressure;
+        newState.temperature = action.data.temperature;
+        newState.time = action.data.time;
+        return { ...state, ...action.data };
+      } catch (err) {
+        console.log(err);
+        return initState;
+      }
     }
     default:
       return state;
