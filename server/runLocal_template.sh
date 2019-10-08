589f30332cc694132e2fee771f1621dce63d9c1d
kvasbo <audun@kvasbo.no>
2019-10-08 14:46:05 +0200

    Add runlocal template

---
+++ server/runLocal_template.sh
@@ -0,0 +1,12 @@
+export NETATMO_USERNAME=x
+export NETATMO_PASSWORD=x
+export NETATMO_CLIENT_SECRET=x
+export NETATMO_CLIENT_ID=x
+export FIREBASE_USER=x
+export FIREBASE_PASSWORD=x
+export TIBBER_KEY=x
+export TIBBER_HOME=x
+export TIBBER_CABIN=x
+export TIMBER_API_KEY=x
+# ./node_modules/.bin/babel src --out-dir dist
+node ./dist/index.js
