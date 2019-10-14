7fd73605739144113218e54e07fbfa38271aca93
kvasbo <audun@kvasbo.no>
2019-10-14 08:37:55 +0200

    Init native app

---
+++ tellulf-app/tellulf-app/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  tellulf-app
+//
+//  Created by Audun Kvasbø on 14/10/2019.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
