ea2a9d0831edb23d30c68c529a3e01e1440dc887
Audun Kvasbø <audun@kvasbo.no>
2020-09-17 18:02:05 +0200

    Move hosting

---
+++ docs/precache-manifest.0afd77695aa67115cf089696f4fd09a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4398da8136f9da3dc55cf4ae7610ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34bc788464c83d24fedb",
+    "url": "/static/css/main.e715a163.chunk.css"
+  },
+  {
+    "revision": "c4adb1e975f9f3f96db5",
+    "url": "/static/js/2.ee3dd2a3.chunk.js"
+  },
+  {
+    "revision": "72bcc2e977dd534e86e930ae05cd64d3",
+    "url": "/static/js/2.ee3dd2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34bc788464c83d24fedb",
+    "url": "/static/js/main.9f15b6a2.chunk.js"
+  },
+  {
+    "revision": "bc44190d25fc9ea9cd75",
+    "url": "/static/js/runtime-main.fc610fb4.js"
+  }
+]);
