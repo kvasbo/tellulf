9a7222096436c77f7443de8db539733590833956
Audun Kvasbø <audun@kvasbo.no>
2021-01-24 13:44:23 +0100

    Update updateTrains.ts

--- client/src/ruter/updateTrains.ts
+++ client/src/ruter/updateTrains.ts
@@ -1,58 +1,57 @@
-import axios from 'axios';
+import createEnturService from '@entur/sdk';
 import Moment from 'moment';
-import XML from 'pixl-xml';
 import { v4 as uuidv4 } from 'uuid';
-import {
-  TrainData,
-  TrainDataSet,
-  EnturApiData,
-  EnturTripData,
-  EnturCallData,
-} from '../types/trains';
+import { TrainData, TrainDataSet } from '../types/trains';
 
 // Create link with unique requestor ID
 const fetchId = uuidv4();
 const ENTUR_URL = `https://api.entur.io/realtime/v1/rest/et?datasetId=RUT&requestorId=${fetchId}`;
 const THE_STOP = 'NSR:Quay:11460';
 
+// const VINDEREN = 'NSR:StopPlace:58270';
+const VINDEREN_T = 'NSR:StopPlace:6245';
+const VINDEREN_B = 'NSR:StopPlace:6248';
+const VINDEREN_T_SPOR = '2';
+const VINDEREN_B_SPOR = '2';
+
+// const SLEMDAL_BUS = 'NSR:StopPlace:58270';
+// const SLEMDAL_BANE = 'NSR:StopPlace:58270';
+
 // Fetch Entur API data
 export default async function getTrains(): Promise<TrainDataSet> {
   const trains: TrainDataSet = {};
   try {
-    const config = { headers: { 'ET-Client-Name': 'kvasbo - infoskjerm' } };
-    const data = await axios.get(ENTUR_URL, config);
-    if (data.status !== 200) throw Error('Couldnt fetch entur data');
-    const parsed: EnturApiData = XML.parse(data.data);
-    // Get the interesting data.
-    const actualData =
-      parsed.ServiceDelivery.EstimatedTimetableDelivery.EstimatedJourneyVersionFrame
-        .EstimatedVehicleJourney;
-    // Empty data set!
-    if (!actualData || !actualData.filter) return trains;
-    // Only the correct station and line
-    const filteredData = actualData.filter((t: EnturTripData) => {
-      return t.DirectionRef === '1' && t.LineRef === 'RUT:Line:1';
-    });
-    filteredData.forEach((t: EnturTripData) => {
-      // Kill bad datas
-      if (
-        !t.EstimatedCalls ||
-        !t.EstimatedCalls.EstimatedCall ||
-        !t.EstimatedCalls.EstimatedCall.length // not an array!
-      )
-        return;
-      t.EstimatedCalls.EstimatedCall.forEach((c: EnturCallData) => {
-        if (c.StopPointRef && c.StopPointRef !== THE_STOP) return;
-        const out: TrainData = {
-          ruteTid: Moment(c.AimedArrivalTime),
-          faktiskTid: Moment(c.ExpectedArrivalTime),
-          id: `${t.FramedVehicleJourneyRef.DataFrameRef}_${t.FramedVehicleJourneyRef.DatedVehicleJourneyRef}`,
-          linje: t.LineRef,
-          skalTil: c.DestinationDisplay,
-        };
-        trains[out.id] = out;
+    // New
+    const entur = createEnturService({ clientName: 'kvasbo-infoskjerm' });
+
+    const trips = await entur.getDeparturesFromStopPlaces([VINDEREN_T, VINDEREN_B]);
+
+    trips?.forEach((t) => {
+      t?.departures.forEach((d) => {
+        // Bane
+        if (
+          d.quay?.stopPlace.id === VINDEREN_T &&
+          d.quay?.publicCode === VINDEREN_B_SPOR &&
+          d.forBoarding
+        ) {
+          console.log(d);
+          const out: TrainData = {
+            ruteTid: Moment(d.aimedArrivalTime),
+            faktiskTid: Moment(d.expectedArrivalTime),
+            id: d.serviceJourney.id,
+            linje: '1',
+            skalTil: d.destinationDisplay.frontText,
+          };
+          trains[out.id] = out;
+        } else if (true) {
+          // console.log(d);
+        }
       });
     });
+
+    const filteredData = [];
+
+    return trains;
   } catch (err) {
     // eslint-disable-next-line no-console
     console.log(err);
