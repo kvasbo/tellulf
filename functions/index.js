964e8c79e2570ef6cb639dd9b5639fa89bd72033
Audun Kvasbø <audun@kvasbo.no>
2021-02-13 22:53:19 +0100

    Re-add functions

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const axios = require('axios');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'false');
+  next();
+});
+
+app.get('/', (req, res) => {
+  console.log('Get', req.query.url);
+  axios
+    .get(req.query.url)
+    .then((data) => {
+      res.send(data.data);
+      return;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+exports.proxy = functions.https.onRequest(app);
