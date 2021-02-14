f6c038ae9d93e52b636f46a074ee9efc638f6016
Audun Kvasbø <audun@kvasbo.no>
2021-02-14 17:57:35 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 import Moment from 'moment';
 import 'moment/locale/nb';
 import React from 'react';
