bde06c64eadd2ed3ba1ddb6c12868c04a198f3b2
Audun Kvasbø <audun@kvasbo.no>
2021-04-07 11:49:15 +0200

    Power price query now uses external component

--- client/src/tibberUpdater.ts
+++ client/src/tibberUpdater.ts
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import Moment from 'moment';
 import Tibber from 'tibber-pulse-connector';
+import { TibberQuery, TibberFeed, IConfig } from 'tibber-api';
 import { TibberSettings } from './App';
 import {
   updateInitStatus,
@@ -17,60 +18,38 @@ const nettleie = 0.477;
 export default class TibberUpdater {
   private store: { dispatch: AppDispatch };
   private settings;
+  private query: TibberQuery;
+  private feed: TibberFeed;
+
   public constructor(store: { dispatch: AppDispatch }, settings: TibberSettings) {
     this.store = store;
     this.settings = settings;
+
+    const config: IConfig = {
+      active: true,
+      apiEndpoint: {
+        apiKey: this.settings.tibberApiKey, // Demo token
+        queryUrl: 'https://api.tibber.com/v1-beta/gql',
+        feedUrl: 'wss://api.tibber.com/v1-beta/gql/subscriptions',
+      },
+    };
+
+    // Instance of TibberQuery
+    this.query = new TibberQuery(config);
+    this.feed = new TibberFeed(config);
   }
 
+  // TODO: PRICE PER HOUSE
   public async updatePowerPrices(): Promise<void> {
-    const settings: TibberSettings = this.settings;
+    const prices = await this.query.getTodaysEnergyPrices(this.settings.tibberHomeKey);
+    const parsedPrices = {};
+    prices.forEach((p: { startsAt: string; total: number }) => {
+      const h = Moment(p.startsAt).hours();
+      parsedPrices[h] = { total: p.total + nettleie };
+    });
 
-    const queryPrices = `
-    {
-      viewer {
-        home(id: "${settings.tibberHomeKey}") {
-          currentSubscription {
-            priceInfo {
-              today {
-                total
-                energy
-                tax
-                startsAt
-              }
-            }
-          }
-        }
-      }
-    }
-`;
-
-    try {
-      const data = await axios({
-        url: 'https://api.tibber.com/v1-beta/gql',
-        method: 'post',
-        headers: {
-          Authorization: `bearer ${this.settings.tibberApiKey}`,
-        },
-        data: {
-          query: queryPrices,
-        },
-      });
-      if (data.status === 200 && data.data.data.viewer.home.currentSubscription.priceInfo.today) {
-        // Parse power prices
-        const prices = data.data.data.viewer.home.currentSubscription.priceInfo.today;
-        const powerPrices = {};
-        prices.forEach((p: { startsAt: string; total: number }) => {
-          const h = Moment(p.startsAt).hours();
-          powerPrices[h] = { total: p.total + nettleie };
-        });
-
-        this.store.dispatch(updatePowerPrices(powerPrices));
-        this.store.dispatch(updateInitStatus('powerPrices'));
-      }
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.log(err);
-    }
+    this.store.dispatch(updatePowerPrices(parsedPrices));
+    this.store.dispatch(updateInitStatus('powerPrices'));
   }
 
   public async updateConsumption(): Promise<void> {
